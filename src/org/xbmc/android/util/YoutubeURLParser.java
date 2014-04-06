a5b95fc47c4abc9b4ed8cca80cdc56ea732c1991
Jon A <jf_archer@yahoo.com>
2014-04-06 21:30:42 +0100

    fixing youtube url parsing

--- src/org/xbmc/android/util/YoutubeURLParser.java
+++ src/org/xbmc/android/util/YoutubeURLParser.java
@@ -5,18 +5,27 @@ import java.util.regex.Pattern;
 
 import android.net.Uri;
 
-public class YoutubeURLParser {
-	public static String parseYoutubeURL(Uri playuri) {
-		if (playuri.getHost().endsWith("youtube.com") || playuri.getHost().endsWith("youtu.be")) {
-			// We'll need to get the v= parameter from the URL and use
-			// that to send to XBMC
-			final Pattern pattern = Pattern.compile("^http(:?s)?:\\/\\/(?:www\\.)?(?:youtube\\.com|youtu\\.be)\\/watch\\?(?=.*v=([\\w-]+))(?:\\S+)?$", Pattern.CASE_INSENSITIVE);
-//			final Pattern pattern = Pattern.compile(".*v=([a-z0-9_\\-]+)(?:&.)*", Pattern.CASE_INSENSITIVE);
-			final Matcher matcher = pattern.matcher(playuri.toString());
-			if (matcher.matches()) {
-				return matcher.group(2);
-			}
-		}
-		return null;
-	}
-}
+ public static String parseYoutubeURL(Uri playuri) {
+        Pattern patregex;
+        
+        if (playuri.getHost().endsWith("youtube.com") == true) {
+            // We'll need to get the v= parameter from the URL and use
+            // that to send to XBMC
+            patregex = Pattern.compile("^http(:?s)?:\\/\\/(?:www\\.)?(?:youtube\\.com)\\/watch\\?(?=.*v=([\\w-]+))(?:\\S+)?$", Pattern.CASE_INSENSITIVE);
+            
+        } else if (playuri.getHost().endsWith("youtu.be") == true) {
+            patregex = Pattern.compile("^http(:?s)?:\\/\\/(?:www\\.)?(?:youtu\\.be)(?=.*\\/([\\w-]+))(?:\\S+)?$", Pattern.CASE_INSENSITIVE);
+                
+        } else {
+            patregex = null;
+            return null;
+        }
+
+        final Pattern pattern = patregex;
+        final Matcher matcher = pattern.matcher(playuri.toString());
+        if (matcher.matches()) {
+            return matcher.group(2);
+        } else {
+            return null;
+        }
+    }
